58ca0f6c245b3ec635eac02ac3779c9a417a6488
cassandraperes <127313906+cassandraperes@users.noreply.github.com>
2024-06-21 19:38:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Qual das seguintes práticas contribui mais positivamente para a redução da pegada de carbono?",
+        alternativas: [
+            "Utilização de energia solar para aquecimento de água.",
+            "Uso de plástico reciclável em embalagens."
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
